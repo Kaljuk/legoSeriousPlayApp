f3997faa9db7b51ed96097d71c617e579d7f46a0
MaikPython <soeoskarpaul@gmail.com>
2018-12-26 00:28:14 +0200

    collabgame

--- legoseriousapp/components/Screens/Game-screen/Question-components/QuestionButton.js
+++ legoseriousapp/components/Screens/Game-screen/Question-components/QuestionButton.js
@@ -5,17 +5,24 @@ import {
     TouchableOpacity
 } from 'react-native';
 
-const QuestionButton = (props) => {
+class QuestionButton extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const enabled = this.props.enabled || false;
+        const callable = (enabled==true && this.props.callable)? this.props.callable : (()=>console.log('question button missing callable'))
+        const color = (enabled == false)? 'gray': '#00BFFF'; //ternary operator
+        const text = (this.props.text)? this.props.text: 'vastus puudub';
         return (
-            <View style={{ backgroundColor: '#00BFFF', width: 170, height: 70, marginTop: 30, justifyContent: 'center'}} >
-                <TouchableOpacity
-                    onPress={() => props.onPress()} 
-                >
-                    <Text style={{color:'white', fontSize: 20, fontFamily: 'Ubuntu', textAlign: 'center'}}>{props.answers}</Text>
+            <View style={{ backgroundColor: color, width: 170, height: 70, marginTop: 30, justifyContent: 'center'}} >
+                <TouchableOpacity onPress={() => callable()}>
+                    <Text style={{color:'white', fontSize: 20, fontFamily: 'Ubuntu', textAlign: 'center'}}>{text}</Text>
                 </TouchableOpacity>
             </View>
         );
     }
+}
 export default QuestionButton;
 
 
