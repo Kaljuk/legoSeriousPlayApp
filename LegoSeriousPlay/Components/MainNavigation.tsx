127192a3a6bbee0f8ec2c0b848ac518e31d6e2c4
Kaljuk <erik.kaljumae@gmail.com>
2019-10-06 15:48:03 +0300

    MainNavigation init

---
+++ LegoSeriousPlay/Components/MainNavigation.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class MainNavigation extends Component {
+  /**
+   *
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <View>
+        <Text>
+          MainNavigation
+        </Text>
+      </View>
+    )
+  }
+}
