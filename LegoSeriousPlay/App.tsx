ed768b76542db6e60a8f05591d4330f408fe0196
MaikPython <soeoskarpaul@gmail.com>
2019-10-06 16:32:55 +0300

    Changes in README

--- LegoSeriousPlay/App.tsx
+++ LegoSeriousPlay/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>THIS IS OUR APP</Text>
     </View>
   );
 }
@@ -15,5 +15,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
